f0218ee7f0b48daf604114d583b212b635467265
brianjckim <bkim2102@gmail.com>
2018-08-02 22:44:15 -0400

    add calendar functionality

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Calendar from "react-big-calendar";
 import moment from "moment";
+import { getFile, putFile } from "blockstack";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
@@ -10,19 +11,39 @@ class SafeCalendar extends Component {
   constructor() {
     super();
     this.state = {
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, "day"))
-        }
-      ]
+      events: []
     };
+    this.saveNewEvent = this.saveNewEvent.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const options = { decrypt: true };
+    let file = await getFile("events.json", options);
+    let events = JSON.parse(file || "[]");
+    console.log(events);
+    this.setState({
+      events
+    });
+  }
+
+  async saveNewEvent(event) {
+    await this.setState({
+      events: [...this.state.events, event]
+    });
+    let events = this.state.events;
+    const options = { encrypt: true };
+    await putFile("events.json", JSON.stringify(events), options);
   }
 
   render() {
     return (
       <div id="calendar-container">
         <Calendar
+          selectable
           defaultDate={new Date()}
           defaultView="week"
           events={this.state.events}
@@ -30,7 +51,14 @@ class SafeCalendar extends Component {
             week: true,
             day: true
           }}
-          color="#6067f1"
+          onSelectSlot={async slotInfo => {
+            let eventName = await prompt("What is the name of the event?");
+            this.saveNewEvent({
+              name: eventName,
+              start: slotInfo.start,
+              end: slotInfo.end
+            });
+          }}
         />
       </div>
     );
