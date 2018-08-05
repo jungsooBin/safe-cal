df8766c5068325de82a6ddf3bbc4d86eac4320aa
Jungsoo Bin <witberrybean@gmail.com>
2018-08-05 15:53:16 -0400

    added history and cleaned up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux'
-
+import history from './history'
 import store from './store'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from "./components/App.jsx";
@@ -11,7 +11,7 @@ import style from "./styles/style.css";
 ReactDOM.render(
   
   <Provider store={store}>
-    <Router>
+    <Router history= {history}>
       <App />
     </Router>
   </Provider>,
